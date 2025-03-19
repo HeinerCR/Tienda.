0fefdb6ace4b12b236e41583c3a93dae44bad867
Heiner Calderón <heinner1124@gmail.com>
2025-03-18 20:42:28 -0600

    Semana 10

--- src/main/java/com/Tienda/controller/IndexController.java
+++ src/main/java/com/Tienda/controller/IndexController.java
@@ -1,6 +1,7 @@
 
 package com.Tienda.controller;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,14 +11,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
 
     @RequestMapping("/")
-    public String page(Model model) {
-        model.addAttribute("attribute", "value");
+    public String index (Model model, HttpSession session) {
+        Long idUsuario = (Long)session.getAttribute ("idUsuario");
         return "index";
-    }
-    
-    @RequestMapping("/informacion")
-    public String contacto(Model model) {
-        return "contacto";
     }
 
 }
